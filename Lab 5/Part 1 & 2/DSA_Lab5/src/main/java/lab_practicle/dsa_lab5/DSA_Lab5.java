dfdf457444f95d4a69b07621db44851891e56dda
ibraralichanna125@gmail.com <ibraralichanna125@gmail.com>
2024-11-14 21:49:53 +0500

    Lab 5

---
+++ Lab 5/Part 1 & 2/DSA_Lab5/src/main/java/lab_practicle/dsa_lab5/DSA_Lab5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lab_practicle.dsa_lab5;
+
+/**
+ *
+ * @author Ibrar Ali
+ */
+public class DSA_Lab5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
